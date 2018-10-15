4e1520c8b1c9651e9af5fa929243da01422ec3c7
Will Hawks <will@creativedistillery.com>
2018-10-15 14:24:39 -0500

    finished module 4 client side graphql

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,38 @@
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates 
+    const updates = { ...args };
+    //remove id from updates (can't update that)
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
+
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. check if they own the item, or have the permissions
+    // TODO
+    // 3. delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
+};
+
+module.exports = Mutations;
